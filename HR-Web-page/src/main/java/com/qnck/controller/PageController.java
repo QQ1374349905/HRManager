626e8e2fd5c93718d17cf46e5d36ff07811d798d
zhangfuwei <1374349905@qq.com>
2021-04-07 16:24:49 +0800

    首次提交代码,初始化模块

---
+++ HR-Web-page/src/main/java/com/qnck/controller/PageController.java
@@ -0,0 +1,12 @@
+package com.qnck.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+    @RequestMapping("toPage")
+    private String toPage(String page) {
+        return page;
+    }
+}
