eb7d3cc4c06ab79b5143ef60e744d44ac12a5241
zhangfuwei <1374349905@qq.com>
2021-04-08 11:27:20 +0800

    添加一个用户数据

--- HR-Dao/src/test/java/com/qnck/AppTest.java
+++ HR-Dao/src/test/java/com/qnck/AppTest.java
@@ -3,6 +3,13 @@ package com.qnck;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.DigestUtils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  * Unit test for simple App.
@@ -13,8 +20,31 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() throws ClassNotFoundException, SQLException {
+        String username = "zhangsan";
+        String realName = "张三";
+        String password = "admin";
+        int identity = 8;
+        System.out.println("加密前:"+password);
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+        System.out.println("加密后:"+password);
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Connection connection = DriverManager.getConnection("jdbc:mysql://10.10.3.231:3306/hr_Manager", "root", "Zhang3@.");
+        System.out.println("获取连接:"+connection);
+
+        PreparedStatement prepareStatement = connection.prepareStatement("insert into User values (null,?,?,?,?)");
+        prepareStatement.setString(1,username);
+        prepareStatement.setString(2,realName);
+        prepareStatement.setString(3,password);
+        prepareStatement.setInt(4,identity);
+
+        int result = prepareStatement.executeUpdate();
+        if (result > 0){
+            System.out.println("插入成功");
+        } else {
+            System.out.println("插入失败");
+        }
     }
 }
