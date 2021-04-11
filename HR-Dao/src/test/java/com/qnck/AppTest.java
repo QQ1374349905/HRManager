653d1a4871201c713fb5bfca4af9d514cca8de2f
zhangfuwei <1374349905@qq.com>
2021-04-11 15:28:51 +0800

    修改数据库IP地址

--- HR-Dao/src/test/java/com/qnck/AppTest.java
+++ HR-Dao/src/test/java/com/qnck/AppTest.java
@@ -10,6 +10,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Date;
 
 /**
  * Unit test for simple App.
