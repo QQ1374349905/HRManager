d5081788b23e13c6c531ba3ff2f251a9240cdf5d
nano <3254854103@qq.com>
2021-04-13 10:44:15 +0800

    新增加添加功能

--- HR-Dao/src/main/java/com/qnck/dao/recruit/Engage_major_releaseDao.java
+++ HR-Dao/src/main/java/com/qnck/dao/recruit/Engage_major_releaseDao.java
@@ -4,7 +4,7 @@ import com.qnck.entity.Engage_major_release;
 import org.apache.ibatis.annotations.Insert;
 
 public interface Engage_major_releaseDao {
-    @Insert("INSERT INTO Config_major(mre_id,first_kind_id,second_kind_id,third_kind_id,\n" +
+    @Insert("INSERT INTO Engage_major_release(mre_id,first_kind_id,second_kind_id,third_kind_id,\n" +
             "major_kind_id,major_id,human_amount,engage_type," +
             "deadline,register,regist_time,major_describe,engage_required) " +
             "VALUES(DEFAULT,#{config_file_first_kinds.first_kind_id},#{config_file_second_kinds.second_kind_id}" +
--- HR-Web-recruit/src/main/java/com/qnck/controller/ConfigMajorController.java
+++ HR-Web-recruit/src/main/java/com/qnck/controller/ConfigMajorController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.sql.Date;
 import java.util.List;
 
 /**
@@ -36,10 +37,13 @@ public class ConfigMajorController {
      * @return
      */
     @RequestMapping("addMajor")
-    public String AddConfigMajor(Engage_major_release engageMajorRelease){
-        System.out.println("______________________________________");
+    public String AddConfigMajor(Config_file_first_kind configFileFirstKind,Config_file_second_kind configFileSecondKind,Config_file_third_kind configFileThirdKind,Config_major configMajor,Engage_major_release engageMajorRelease){
+        engageMajorRelease.setConfig_file_first_kinds(configFileFirstKind);
+        engageMajorRelease.setConfig_file_third_kinds(configFileThirdKind);
+        engageMajorRelease.setConfig_file_third_kinds(configFileThirdKind);
+        engageMajorRelease.setConfig_major(configMajor);
         engageMajorReleasesService.addEngage_major_release(engageMajorRelease);
-        return "toPage?page=page/recruit/position/position_change_update.html";
+        return "redirect:toPage?page=page/recruit/position/position_change_update.html";
 
     }
 }
